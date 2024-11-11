8d6e5025a2f8354e2ebba330de192ec68d3c1455
BenVN123 <bnguyen123.vn@gmail.com>
2024-11-10 20:50:31 -0800

    clang-format for cpp

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -3,13 +3,14 @@ return {
 		"stevearc/conform.nvim",
 		opts = {
 			formatters_by_ft = {
-				python = { "black" }, -- Example formatter for Python
-				c = { "clang-format" }, -- Example formatter for C
+				python = { "black" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
 				lua = { "stylua" },
 			},
 
-			format_on_save = true, -- Enable auto-formatting on save
-			lsp_fallback = true, -- Use LSP formatter as a fallback if no formatter is configured
+			format_on_save = true,
+			lsp_fallback = true,
 		},
 	},
 }
