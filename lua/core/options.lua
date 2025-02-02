aaf38b4046fdad02220d0f6d3f20c84c27e93df9
Ben Nguyen <bnguyen123.vn@gmail.com>
2025-02-02 14:35:06 -0800

    i am primeagen

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,4 +4,6 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
-vim.cmd.colorscheme("catppuccin")
+-- vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("rose-pine")
+-- vim.cmd.colorscheme("rose-pine-moon")
